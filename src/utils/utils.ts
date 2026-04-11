40a193eb3855128763eb7d8eaae183b737973db1
ruwusty <russelljiang@pm.me>
2026-04-12 09:34:35 +1000

    chore: restore blog slide constants in utils.ts

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -19,6 +19,21 @@ export const FYG_2025_LINK =
 "https://drive.google.com/file/d/1wR9QEjfXN083SalIQWsshq-KWSPsXtmj/preview";
 
 
+export const DATA_SCI_BLOG =
+"https://docs.google.com/presentation/d/e/2PACX-1vSompDUNtS_lM1clzxcuK8Ho1fKYXcpNPBzCpqi0pjYQiLs59O80lvx5chsbvKOnD9CKy_bk4tfJckT/pubembed?start=false&loop=false&delayms=3000";
+export const DATA_VISUALIZATION =
+"https://docs.google.com/presentation/d/e/2PACX-1vQB2dIY0uV4x9sDmrX0sRwzTXaEGEjigiFWXF2Ld7ZsYBohVP1p7-CVkOTrS9vPeXBZ-Pp4Nz3vsA5c/pubembed?start=false&loop=false&delayms=3000";
+export const ETHICS_DATA_SCIENCE =
+"https://docs.google.com/presentation/d/e/2PACX-1vR6UKGQBBPmUgcKYDIexbITOebtbDzcobRLPR3mvTsY1UdAFfjzbaf6grbLwcY-XGC4aA_oTYbOK_2y/pubembed?start=false&loop=false&delayms=3000";
+export const EXPLAINABILITY =
+"https://docs.google.com/presentation/d/e/2PACX-1vRCWsMop02lL1Q9tSKdQ3Do-6QiWEvD0lAhA802nlt9lXK-4BvkXknmcM1AFSfiMGL04iozflNizlFf/pubembed?start=false&loop=false&delayms=3000";
+export const MACHINE_LEARNING =
+"https://docs.google.com/presentation/d/e/2PACX-1vQmKe-XUrIs8DfgCdCaSH_5Ui_vYiFLQ5R3YuXrecZkL1EI3wejyu1o1rCHNxFz-Qw7mIMlSRlwR1Wk/pubembed?start=false&loop=false&delayms=3000";
+export const ORLANDO =
+"https://docs.google.com/presentation/d/e/2PACX-1vRk10746heyAM_1yNxb6_I2XO_I6yRij83m8ylpNknfUpg45Mc2_KAltISljWy2iTcXrtVrfRZ5LVW5/pubembed?start=false&loop=false&delayms=3000";
+export const SENTIMENTAL_ANALYSIS =
+"https://docs.google.com/presentation/d/e/2PACX-1vRhRjVi0aFYkS4l46Hm1LsrSpbOjA3Nv08pcNnB2nOTeEHFSziT4gQQoQa6eYqMG4DqtljlUyBZg_KS/pubembed?start=false&loop=false&delayms=3000";
+
 export const CAREERS_GUIDE =
 "https://drive.google.com/file/d/1iBsJdAYltRyh9b3eBlt9pkXC1gPPwo1c/preview";
 export const CAREERS_GUIDE_2021 =
